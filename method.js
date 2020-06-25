c002b694cada2e9bc68425ef9b9341a384825127
YaqoobAslam <happyyaqoob@gmail.com>
2020-06-25 07:10:05 -0700

    obect and method

---
+++ method.js
@@ -0,0 +1,28 @@
+const obj = {
+    foo(){
+        return 'bar';
+    }
+};
+
+console.log(obj.foo());
+
+//----------------------------------
+const obj ={
+    foo: function(){
+
+    },
+    bar: function(){
+        
+    }
+}
+
+//------------------------------------
+
+const obj = {
+    foo(){
+
+    },
+    bar(){
+
+    }
+}
