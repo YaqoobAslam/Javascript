b97cbc030cf7a672ccba8cd7f16ee00bf40f9749
YaqoobAslam <happyyaqoob@gmail.com>
2020-06-25 00:58:18 -0700

    arguments function

--- rest.js
+++ rest.js
@@ -1,5 +1,29 @@
-function foo(...args){
-    return args;
+// function foo(...args){
+//     return args;
+// }
+
+// console.log(foo(1,2,3));
+
+// //---------------------------------
+// function func(a){
+//     arguments[0] = 99;
+//     console.log(a);
+// }
+
+// func(10);
+//---------------------------------
+function func(a =55){
+    a= 99; 
+    console.log(arguments[0]);
 }
 
-console.log(foo(1,2,3));
+func(10);
+
+//---------------------------------
+
+function func(a = 55){
+
+    console.log(arguments[0]);
+}
+
+func();
