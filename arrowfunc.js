c1e2b97a80fcac505078f6576e012a74dce3fc8c
YaqoobAslam <happyyaqoob@gmail.com>
2020-06-25 04:18:58 -0700

    arrrow function

--- arrowfunc.js
+++ arrowfunc.js
@@ -105,7 +105,46 @@ var adder = {
 console.log(adder.add(1));
 console.log(adder.addThruCall(1));
 
+//-----------------------------------------
+
+var Foo = () =>{};
+var foo = new Foo();
+
+//------------------------------------------
 
 
+//Arrow functions do not have a prototype property.
+var Foo = () =>{};
+console.log(Foo.prototype);
 
+//------------------------------------------
+var func = (x,y) =>{
+    return x + y;
+}
+//-------------------------------------------
+
+var simple = a => a > 15 ? 15 : a;
+simple(16);
+simple(10);
+
+//--------------------------------------------
+
+let max = (a,b) => a > b ? a: b;
+
+
+var arr = [5,6,13,0,1,18,23];
+
+var sum = arr.reduce((a,b) => a + b);
+
+
+var even = arr.filter(v => v%2 ==0);
+
+
+var double = arr.map(v => v * 2);
+
+Promise.then(a =>{
+
+}).then(b =>{
+
+});
 
