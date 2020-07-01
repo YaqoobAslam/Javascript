4720f634bef3a0fa85c25140384bbb2baea64055
YaqoobAslam <YaqoobAslam@users.noreply.github.com>
2020-07-01 04:12:19 -0700

    Async Funtion and await
    Async Funtion and await

--- async.js
+++ async.js
@@ -8,7 +8,9 @@ function resolveAfter2Seconds(){
     });
 }
 
-const { reject } = require("async")
+const { concatSeries } = require("async");
+
+const { reject, compose } = require("async")
 
 async function asyncCall(){
     console.log('calling');
@@ -32,6 +34,7 @@ console.log('File Closed!');
 //-------------------------------------------------------
 //synchronous function
 const fs = require('fs');
+const { resolve } = require("path");
 try{
      const data = fs.readFileSync(__dirname + '/read.txt','utf8');
      console.log(data.toString());
@@ -115,4 +118,125 @@ async function foo(){
      const p2 = new Promise((_,reject)=> setTimeout(()=>reject('2'),500))
      const results = [await p1,await p2]
 }
-foo().catch(()=>{}) 
+foo().catch(()=>{})
+//--------------------------------------------------------
+
+function resolveAfter2Seconds(){
+     console.log("starting slow promise")
+     return new Promise(resolve =>{
+          setTimeout(function(){
+               resolve("slow")
+               console.log("slow promise is done")
+          },2000)
+     })
+}
+
+function resolveAfter2Seconds(){
+     console.log("starting fast promise")
+     return new Promise(resolve =>{
+          setTimeout(function(){
+               resolve("fast")
+               console.log("fast promise is done")
+          },1000)
+     })
+}
+
+async function sequentialStart(){
+     console.log('==SEQUENTIAL START==')
+     const slow = await resolveAfter2Seconds()
+     console.log(slow)
+
+     const fast = await resolveAfter1Seconds()
+     console.log(fast)
+}
+
+async function concurrentStart(){
+     console.log('==CONCURRENT START with await');
+     const slow = resolveAfter2Seconds()
+     const fast = resolveAfter1Seconds()
+
+     console.log(await slow)
+     console.log(await fast)
+
+}
+
+function concurrentPromise(){
+     console.log('==CONCURRENT START with Promise.all==')
+     return Promise.all([resolveAfter2Seconds(),resolveAfter1Seconds()]).then((message)=>{
+          console.log(message[0])
+          console.log(message[1])
+     })
+}
+
+async function parallel(){
+     console.log('==PARALLEL with await Promise.all==')
+     
+     await Promise.all([
+          (async()=>console.log(await resolveAfter2Seconds()))(),
+          (async()=>console.log(await resolveAfter1Seconds()))()
+     ])
+
+}
+
+sequentialStart()
+setTimeout(concurrentStart, 4000)
+setTimeout(concurrentPromise,7000)
+setTimeout(parallel,10000)
+//------------------------------------------------
+
+function getProcessedData(url){
+     return downloadData(url)
+     .catch(e =>{
+          return downloadFallbackData(url)
+     })
+     .then(v =>{
+          return getProcessDataInWorker(v)
+     })
+}
+//-------------------------------------------------
+
+async function getProcessedData(url){
+     let v
+     try{
+          v=await downloadData(url)
+     }catch(e){
+          v = await downloadFallbackData(url)
+     }
+     return getProcessDataInWorker(v)
+}
+//-------------------------------------------------
+async function getProcessedData(url){
+     let v
+     try{
+          v = await downloadData(url)
+     }catch(e){
+          v = await downloadFallbackData(url)
+     }
+     try{
+          return await pocessDataInWorker(v)
+     }catch(e){
+          return null
+     }
+}
+
+//-------------------------------------------------
+
+async function harry(){
+     console.log("Inside the harry function")
+     const response = await fetch('https://api.github.com/users');
+     console.log('Before response');
+     const users = await response.json();
+     console.log('user resolved')
+     return users;
+
+     // return "harry";
+}
+
+console.log("Before calling harry")
+let a = harry();
+console.log("After calling harry")
+console.log(a);
+a.then(data =>console.log(data))
+console.log("Last line of this file")
+
+//-------------------------------------------------
